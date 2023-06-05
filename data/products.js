02d46769cc030e440f97de6baab9a3da608353cc
Nathalie <nat933@free.fr>
2023-06-05 16:42:56 +0200

    update home

--- data/products.js
+++ data/products.js
@@ -1,8 +1,9 @@
+
 export const products = [
     {
         id: 1,
         title: "accusamus beatae ad facilis cum similique qui sunt",
-        image: "./images/image1.jpg",
+        image: "/images/image1.jpg",
         description: "2011 sees the celebration of the 400th anniversary of the publication of the King James Version of the Bible (or Authorized Version). The KJV is a strong contender for the accolade of 'the book that has had more influence on the development of English than any other'.",
         price: 100,
         category: "Carte blanche",
@@ -11,7 +12,7 @@ export const products = [
     {
         id: 2,
         title: "accusamus beatae ad facilis cum similique qui sunt",
-        image: "./images/image2.jpg",
+        image: "/images/image2.jpg",
         description: "2011 sees the celebration of the 400th anniversary of the publication of the King James Version of the Bible (or Authorized Version). The KJV is a strong contender for the accolade of 'the book that has had more influence on the development of English than any other'.",
         price: 100,
         category: "Carte blanche",
@@ -21,7 +22,7 @@ export const products = [
     {
         id: 3,
         title: "accusamus beatae ad facilis cum similique qui sunt",
-        image: "./images/image3.jpg",
+        image: "/images/image3.jpg",
         description: "2011 sees the celebration of the 400th anniversary of the publication of the King James Version of the Bible (or Authorized Version). The KJV is a strong contender for the accolade of 'the book that has had more influence on the development of English than any other'.",
         price: 100,
         category: "Carte blanche",
@@ -31,7 +32,7 @@ export const products = [
     {
         id: 4,
         title: "accusamus beatae ad facilis cum similique qui sunt",
-        image: "./images/image4.jpg",
+        image: "/images/image4.jpg",
         description: "2011 sees the celebration of the 400th anniversary of the publication of the King James Version of the Bible (or Authorized Version). The KJV is a strong contender for the accolade of 'the book that has had more influence on the development of English than any other'.",
         price: 100,
         category: "Carte blanche",
@@ -41,7 +42,7 @@ export const products = [
     {
         id: 5,
         title: "accusamus beatae ad facilis cum similique qui sunt",
-        image: "./images/image5.jpg",
+        image: "/images/image5.jpg",
         description: "2011 sees the celebration of the 400th anniversary of the publication of the King James Version of the Bible (or Authorized Version). The KJV is a strong contender for the accolade of 'the book that has had more influence on the development of English than any other'.",
         price: 100,
         category: "Carte blanche",
@@ -51,7 +52,7 @@ export const products = [
     {
         id: 6,
         title: "accusamus beatae ad facilis cum similique qui sunt",
-        image: "./images/image6.jpg",
+        image: "/images/image6.jpg",
         description: "2011 sees the celebration of the 400th anniversary of the publication of the King James Version of the Bible (or Authorized Version). The KJV is a strong contender for the accolade of 'the book that has had more influence on the development of English than any other'.",
         price: 100,
         category: "Carte blanche",
